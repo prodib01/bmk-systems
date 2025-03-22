8551af99e126e5fcdce9b95e1404546328464f35
Kisakye Brendah Prosper <kskbrendah@gmail.com>
2025-03-22 13:41:12 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/style.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
